2bb0b321187fa20c05e4fa2da63282c1fb245201
Michael0424 <0424michael@gmail.com>
2023-10-16 12:15:02 +0800

    done

--- src/main/java/com/example/Circle.java
+++ src/main/java/com/example/Circle.java
@@ -2,17 +2,23 @@ package com.example;
 
 public class Circle extends Shape {
 
+    private double radius;
+
+    public Circle(double radius){
+        this.radius=radius;
+    }
+
     // TODO: Define attributes for the circle
 
     @Override
     public double area() {
         // TODO: Implement the method
-        return 0;
+        return Math.PI * radius * radius;
     }
 
     @Override
     public double perimeter() {
         // TODO: Implement the method
-        return 0;
+        return 2 * Math.PI * radius;
     }
 }
--- src/main/java/com/example/Course.java
+++ src/main/java/com/example/Course.java
@@ -1,15 +1,36 @@
 package com.example;
 
-import java.util.List;
+import java.util.ArrayList;
+
 
 public class Course {
     private String courseName;
     private String courseID;
-    private List<Student> students;
+    private ArrayList<Student> students;
 
     public Course(String courseName, String courseID) {
+        this.courseName = courseName;
+        this.courseID = courseID;
+        this.students = new ArrayList<Student>();
+    }
+    public String getCourseName(){
+        return courseName;
+    }
+    public String getCourseID(){
+        return courseID;
+    }
+    public ArrayList <Student> getStudents(){
+        return students;
+    }
+    public void enrollStudent(Student student) {
+        students.add(student);
+    }
+
+    public void dropStudent(Student student) {
+        students.remove(student);
+    }
         // TODO: Initialize the attributes
     }
 
     // TODO: Implement methods to enroll a student and drop a student
-}
+
